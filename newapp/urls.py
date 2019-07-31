82ae5830e8da370d13805aff337c70686f9b7308
Nunyapa <halo.cola.115@gmail.com>
2019-07-31 17:23:10 +1000

    Separate content and index html files. Added file for mainpage,

--- newapp/urls.py
+++ newapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.newview, name="newview"),
+    path('', views.mainpage, name="mainpage"),
     path('pikabu/', views.pikabutake, name="pikabutake"),
-    path('<int:pk>/delete/', views.deleterecord, name="deleterecord"),
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -4,31 +4,16 @@ from .forms import WishForm
 from .models import Wish
 import requests
 # Create your views here.
-
-def newview(request):
-	wishes = Wish.objects.all()
-	if request.method == "POST":
-		form = WishForm(request.POST)
-		if form.is_valid():
-			post = form.save(request.POST)
-	form = WishForm()
-	context = {"form": form, "wishes": wishes}
-	return render(request, 'newapp/index.html', context)
-
-def deleterecord(request, pk):
-	to_delete = get_object_or_404(Wish, pk=pk)
-	to_delete.delete()
-	wishes = Wish.objects.all()
-	form = WishForm()
-	context = {"form": form, "wishes": wishes}
-	HttpResponseRedirect('/')
-	return render(request, 'newapp/index.html', context)
+def mainpage(request):
+	welcome_text = "Hello and welcome to mySite. Add more text to see what happened. Теперь пишу по русски потому что можу"
+	context = {"welcome_text": welcome_text}
+	return render(request, "newapp\welcome.html", context)
 
 def pikabutake(request):
 	app_token = "1e732df61e732df61e732df6c61e18fb5b11e731e732df6434546886b716328dd5fd619"
 	domain = "pikabu"
-	count = 9
-	offset = 1
+	count = 10
+	offset = 0
 	v = 5.101
 	response = requests.get("https://api.vk.com/method/wall.get?",
 		{"domain": domain,
@@ -59,4 +44,4 @@ def pikabutake(request):
 
 	#context = {"text_data": text_data, "photo_url": photo_url, "amount_of_data": amount_of_data}
 	context = {"data": data_list}
-	return render(request, "newapp/index.html", context)
+	return render(request, "newapp/content.html", context)
