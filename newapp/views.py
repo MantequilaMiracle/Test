9c71a900b3a208b7aa5310746dc586f100957fc1
Nunyapa <halo.cola.115@gmail.com>
2019-08-01 13:10:37 +1000

    Security fix

--- newapp/views.py
+++ newapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .forms import WishForm
 from .models import Wish
 import requests
+from .app_token import app_token
 # Create your views here.
 def mainpage(request):
 	welcome_text = "Hello and welcome to mySite. Add more text to see what happened. Теперь пишу по русски потому что можу"
@@ -10,8 +11,43 @@ def mainpage(request):
 	return render(request, "newapp\welcome.html", context)
 
 def pikabutake(request):
-	app_token = "1e732df61e732df61e732df6c61e18fb5b11e731e732df6434546886b716328dd5fd619"
 	domain = "pikabu"
+	count = 10
+	offset = 0
+	v = 5.101
+	response = requests.get("https://api.vk.com/method/wall.get?",
+		{"domain": domain,
+		"count": count,
+		"offset": offset,
+		"access_token": app_token,
+		"v": v
+		})
+	json_data = response.json()["response"]["items"]
+	#text_data = []
+	#photo_url = []
+	data_list = []
+	for data in json_data:
+		text_str = data["text"]
+		photo_list = []
+		#photo_for_text = []
+		try:
+			for attachment in data["attachments"]:
+				if attachment["type"] == "photo":
+					photo_for_the_text = attachment["photo"]["sizes"][-1]["url"]
+					photo_list.append(photo_for_the_text)
+				else:
+					continue
+		except KeyError:
+			continue
+		#photo_url.append(photo_for_text)
+		data_list.append({"text_data": text_str, "photo_url": photo_list})
+
+	#context = {"text_data": text_data, "photo_url": photo_url, "amount_of_data": amount_of_data}
+	context = {"data": data_list}
+	return render(request, "newapp/content.html", context)
+
+def posts(request, publicname, filters):
+	domain = publicname
 	count = 10
 	offset = 0
 	v = 5.101
