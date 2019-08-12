d987ba47ba7d3773cb52fde389ca891825cf6e48
Nunyapa <halo.cola.115@gmail.com>
2019-08-12 15:16:21 +1000

    Login and reset from added

--- newapp/forms.py
+++ newapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class PostForm(forms.Form):
-    public = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={"placeholder": " Vk public domain", "class": "row-1"}))
+    public = forms.CharField(max_length = 30, widget=forms.TextInput(attrs={"placeholder": " Vk public domain", "class": "row-1"}))
     today_posts = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={}))
     offset = forms.IntegerField(required=False, min_value=0, widget=forms.NumberInput(attrs={"placeholder":" default is 0", "class": "row-2"}))
     count = forms.IntegerField(required=False, min_value=0, max_value=100, widget=forms.NumberInput(attrs={"placeholder":" default is 10", "class": "row-3"}))
