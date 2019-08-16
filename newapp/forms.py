3c881d4c22f57eae1d3ba71527f37e60e9e1d423
Nunyapa <halo.cola.115@gmail.com>
2019-08-16 18:02:10 +1000

    added alt for gifs and imges

--- newapp/forms.py
+++ newapp/forms.py
@@ -2,7 +2,16 @@ from django import forms
 from django.contrib.auth.models import User
 
 class PostForm(forms.Form):
-    public = forms.CharField(max_length = 50, widget=forms.TextInput(attrs={"placeholder": " Vk public domain", "class": "row-1"}))
+    public = forms.CharField(label="publics", max_length = 50, widget=forms.TextInput(attrs={
+    "placeholder": " Vk public domain",
+    "class": "row-1"
+    }))
+    offset = forms.IntegerField(required=False, min_value=0, widget=forms.NumberInput(attrs={
+    "placeholder":" default is 0",
+    "class": "row-2"
+    }))
+    count = forms.IntegerField(required=False, min_value=0, max_value=100, widget=forms.NumberInput(attrs={
+    "placeholder":" default is 10",
+    "class": "row-3"
+    }))
     today_posts = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={}))
-    offset = forms.IntegerField(required=False, min_value=0, widget=forms.NumberInput(attrs={"placeholder":" default is 0", "class": "row-2"}))
-    count = forms.IntegerField(required=False, min_value=0, max_value=100, widget=forms.NumberInput(attrs={"placeholder":" default is 10", "class": "row-3"}))
