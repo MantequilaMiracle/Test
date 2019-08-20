61a7bdb3d69b3d3c7036dcddd0a32e81a55bcbcc
Nunyapa <halo.cola.115@gmail.com>
2019-08-20 15:05:58 +1000

    View and template changes

--- newapp/views.py
+++ newapp/views.py
@@ -22,77 +22,81 @@ def registerview(request):
 				form.save()
 				return HttpResponseRedirect("/accounts/login/")
 		except ValueError as e:
-			err_message = "{}: ".format(e)
+			err_message = "Error: {}".format(e)
 			context = {'form': form, "err": err_message}
 	context = {'form': form, "err": ''}
 	return render(request, 'registration/register.html', context)
 
 
-def posts(request, form):
+def post(domain, today_posts, count=10, offset=0):
 	one_day_seconds = 86400
-	total_context = []
-	if form.is_valid():
-		# max count of domain is 3
-		domains = {domain.replace(' ','') for domain in form.cleaned_data["public"].split(',') if domain != ''}#[:3]
-		offset = 0 if form.cleaned_data["offset"] == None else form.cleaned_data["offset"]
-		count = 10 if form.cleaned_data["count"] == None else form.cleaned_data["count"]
-		if form.cleaned_data["today_posts"]:
-			count = 100
-		for domain in domains:
-			data_list = []
-			response = requests.get("https://api.vk.com/method/wall.get?",
-				{"domain": domain,
-				"count": count,
-				"offset": offset,
-				"access_token": app_token,
-				"v": app_version
-				})
-			try:
-				json_data = response.json()["response"]["items"]
-			except KeyError:
-				#TODO: do nothing if some troubles with json
-				text_str = "KeyError in " + domain
-				data_list.append({"text_data": text_str, "photo_url": [], "doc_url": [], "video_url": []})
-				context = {"data": data_list}
-				total_context.append(context)
+	context = {}
+	if today_posts:
+		count = 100
+	data_list = []
+	response = requests.get("https://api.vk.com/method/wall.get?",
+		{"domain": domain,
+		"count": count,
+		"offset": offset,
+		"access_token": app_token,
+		"v": app_version
+		})
+	try:
+		json_data = response.json()["response"]["items"]
+	except KeyError as e:
+		print(e)
+		data_list.append({"text_data": "Check the domain name", "photo_url": [], "doc_url": [], "video_url": []})
+		context = {"data": data_list}
+		return context
+	owner_id = str(json_data[0]["owner_id"])
+	for data in json_data:
+		if today_posts:
+			if data["date"] < (int(time.time())-one_day_seconds):
 				continue
-			owner_id = str(json_data[0]["owner_id"])
-			for data in json_data:
-				if form.cleaned_data["today_posts"]:
-					if data["date"] < (int(time.time())-one_day_seconds):
-						continue
-				text_str = data["text"]
-				photo_list = []
-				video_list = []
-				doc_list = []
-				try:
-					for attachment in data["attachments"]:
-						if attachment["type"] == "photo":
-							temp = attachment["photo"]["sizes"][-1]["url"]
-							photo_list.append(temp)
-						elif attachment["type"] == "doc":
-							temp = attachment["doc"]["url"]
-							doc_list.append(temp)
-						elif attachment["type"] == "video":
-							#https://vk.com/video+owner_id+_+456288678
-							video_id = str(attachment["video"]["id"])
-							temp = "https://vk.com/video" + owner_id + "_" + video_id
-							video_list.append(temp)
-						#TODO: VIDEO
-						else:
-							continue
-				except KeyError:
+		text_str = data["text"]
+		photo_list = []
+		video_list = []
+		doc_list = []
+		try:
+			for attachment in data["attachments"]:
+				if attachment["type"] == "photo":
+					temp = attachment["photo"]["sizes"][-1]["url"]
+					photo_list.append(temp)
+				elif attachment["type"] == "doc":
+					temp = attachment["doc"]["url"]
+					doc_list.append(temp)
+				elif attachment["type"] == "video":
+					#https://vk.com/video+owner_id+_+456288678
+					video_id = str(attachment["video"]["id"])
+					temp = "https://vk.com/video" + owner_id + "_" + video_id
+					video_list.append(temp)
+				#TODO: VIDEO
+				else:
 					continue
-				data_list.append({"text_data": text_str, "photo_url": photo_list, "doc_url": doc_list, "video_url": video_list})
-			context = {"data": data_list, "domain": domain}
-			total_context.append(context)
-	return total_context
+		except KeyError as e:
+			print(e)
+			continue
+		data_list.append({"text_data": text_str, "photo_url": photo_list, "doc_url": doc_list, "video_url": video_list})
+	context = {"data": data_list, "domain": domain}
+	return context
 
 
 def multipost(request):
 	form = PostForm()
 	context = {}
+	total_context = []
 	if request.method == "POST":
 		form = PostForm(request.POST)
-		context = posts(request, form)
-	return render(request, "newapp/content.html", {"total_context": context, "form": form})
+		if form.is_valid():
+			domain_counter = 0
+			domains = {domain.replace(' ','') for domain in form.cleaned_data["public"].split(',') if domain != ''}
+			count = form.cleaned_data["count"]
+			offset = form.cleaned_data["offset"]
+			today_posts = form.cleaned_data["today_posts"]
+			for domain in domains:
+				if domain_counter > 3:
+					domains.clear()
+					break
+				total_context.append(post(domain, today_posts, count, offset))
+				domain_counter += 1
+	return render(request, "newapp/content.html", {"total_context": total_context, "form": form})
