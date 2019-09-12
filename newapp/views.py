66075fcd70ed8f5e82adc46a9dc7af96935e2b8d
Nunyapa <halo.cola.115@gmail.com>
2019-09-12 11:30:46 +1000

    Template changes

--- newapp/views.py
+++ newapp/views.py
@@ -26,7 +26,9 @@ ONE_DAY_SECONDS = 86400
 
 def mainpage(request):
 	#TODO: show suggestions what domains to use
-	context = {"text": "Hello and Welcome!"}
+	context = {"text": '''Web site
+	Trying to create web site with purpose to practice and get knowledge how does it work.
+	Also I want to study Django how the most popular framework for site creature.'''}
 	return render(request, "newapp/mainpage.html", context)
 
 
